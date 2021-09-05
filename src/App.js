87bc83cff454d2ac88312c7c51ca9a0c23b43c58
Jayakrishnan M <Jayakrishnan1012@gmail.com>
2021-09-06 00:01:02 +0530

    Added Widget Section

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ import Widgets from './Widgets';
 function App() {
   return (
     <div className="app">
-
-    {/* SideBar */}
     <Sidebar/>
-    {/* Feed */}
     <Feed />
-    {/* Widgets */}
     <Widgets />
     </div>
   );
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,36 @@
+import { Search } from '@material-ui/icons'
 import React from 'react'
 import './Widgets.css'
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
 
 function Widgets() {
     return (
         <div className="widgets">
-            <h2>Widgets</h2>
+            <div className="widgets__input">
+                <Search className="widgets__searchIcon"></Search>
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+
+            <div className="widgets__widgetContainer">
+                <h2>What's happening</h2>
+
+                <TwitterTweetEmbed tweetId={"1434173909163708422"} />
+
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="ManUtd"
+                    options={{ height: 400 }}
+                />
+
+                <TwitterShareButton
+                    url={"https://www.facebook.com/profile.php?id=100009589212408%22https%3A%2F%2Ffacebook.com%2FjayakrishnanMurali"}
+                    options={{ text: "#reactjs is awesome", via: "Jkay" }}
+                />
+            </div>
         </div>
     )
 }
